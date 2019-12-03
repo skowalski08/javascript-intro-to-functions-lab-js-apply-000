236f11d3476319731bd02e9023a7ecddf1c3bfe1
Sara Kowalski <sarakowalski08@gmail.com>
2019-12-03 21:59:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,3 +18,10 @@ function sayHiToGrandma(string) {
   return "I can't hear you!";
 }
 
+function sayHiToGrandma(string) {
+  return 'YES INDEED!';
+}
+
+function sayHiToGrandma(string) {
+  return 'I love you, too.';
+}
