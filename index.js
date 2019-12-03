847ac69c7000fd6a9f2355adc952267678a4bebe
Sara Kowalski <sarakowalski08@gmail.com>
2019-12-03 21:43:32 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(string) {
+  return string.toUpperCase();
+}
+
+function whisper(string) {
+  return string.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  return "I can't hear you!";
+}
+
+function sayHiToGrandma(string) {
+  return 'YES INDEED!';
+}
+
+function sayHiToGrandma(string) {
+  return 'I love you, too.';
+}
